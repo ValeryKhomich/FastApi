12cbf1e181e388c10693ab27b9c8d82a672111fc
khomich_valery <rafffik@mail.ru>
2024-12-19 13:10:53 +0300

    Added Poetry

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from fastapi.responses import FileResponse, JSONResponse
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list[str] = []
+
+
+@app.post("/items/")
+async def create_item(item: Item) -> Item: # тут мы передали в наш обработчик Pydantic модель, чтобы она проверяла все запросы на соответствие этой модели (все поля и типы данных в них должны соответствовать модели
+    return item
+
+
+@app.get("/items/")
+async def read_items() -> list[Item]: # тут мы не принимаем никаких данных, но указываем, что возвращаться будет список, содержащий в себе Pydantic модели
+    return [
+        Item(name="Portal Gun", price=42.0),
+        Item(name="Plumbus", price=32.0),
+    ]
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
