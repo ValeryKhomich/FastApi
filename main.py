4704d9fc2f96f6a1dd10165de0619bddb2971809
khomich_valery <rafffik@mail.ru>
2024-12-26 13:29:49 +0300

    We're starting new project

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
 import uvicorn
-from fastapi import FastAPI
-from fastapi.templating import Jinja2Templates
-from fastapi.requests import Request
-from fastapi.responses import FileResponse, JSONResponse
-from pydantic import BaseModel
-
+from fastapi import FastAPI, Depends, HTTPException, status
 
 app = FastAPI()
 
+# Функция-зависимость
+def verify_token(token: str):
+    if token != "supersecrettoken":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+        )
+    return {"user": "authorized_user"}
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    tags: list[str] = []
-
-
-@app.post("/items/")
-async def create_item(item: Item) -> Item: # тут мы передали в наш обработчик Pydantic модель, чтобы она проверяла все запросы на соответствие этой модели (все поля и типы данных в них должны соответствовать модели
-    return item
-
-
-@app.get("/items/")
-async def read_items() -> list[Item]: # тут мы не принимаем никаких данных, но указываем, что возвращаться будет список, содержащий в себе Pydantic модели
-    return [
-        Item(name="Portal Gun", price=42.0),
-        Item(name="Plumbus", price=32.0),
-    ]
+# Эндпоинт с зависимостью
+@app.get("/protected/")
+async def protected_route(user: dict = Depends(verify_token)):
+    return {"message": "Access granted", "user": user}
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run("main:app", reload=True)
