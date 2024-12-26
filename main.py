31822fac70101a6c1f6a63d329cf6e6d37cfe9b8
khomich_valery <rafffik@mail.ru>
2024-12-26 13:31:27 +0300

    Second

--- main.py
+++ main.py
@@ -4,18 +4,7 @@ from fastapi import FastAPI, Depends, HTTPException, status
 app = FastAPI()
 
 # Функция-зависимость
-def verify_token(token: str):
-    if token != "supersecrettoken":
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token",
-        )
-    return {"user": "authorized_user"}
 
-# Эндпоинт с зависимостью
-@app.get("/protected/")
-async def protected_route(user: dict = Depends(verify_token)):
-    return {"message": "Access granted", "user": user}
 
 
 if __name__ == '__main__':
